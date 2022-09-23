66d7520284e7f0f9902ba2bdb59cd4dadf40bd5a
thatdanield <thatdanield@gmail.com>
2022-09-23 10:12:48 -0500

    举头望明月

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 /**
  * This ArrayStack object represents a Stack ADT implemented as
@@ -8,7 +9,7 @@
  */
 public class ArrayStack<T> implements StackInterface<T>
 {
-    private int size;
+    private int size = 0;
     private T[] stack;
 
     public ArrayStack()
@@ -16,49 +17,126 @@ public class ArrayStack<T> implements StackInterface<T>
         // cannot create a generic array object, so has to be cast
         // from an Object back into the generic in order to compile
         stack = (T[]) new Object[1];
+        size = 0;
     }
 
     // returns the logical size of the stack
     public int size()    
     {
-        return -1;
+        if ((stack.length<=1)&&(stack[0]==null)&&(size == 0)) {
+            size = 0;
+            return 0;
+        }
+        for (int i = 0; i < stack.length; i++) {
+            if (stack[i]==null) {
+                size = i;
+                return i;
+            }
+        }
+        return stack.length;
     }
 
+    private void checkSize() {
+        int newSize;
+        if (size>=stack.length) {
+            newSize = stack.length*2;
+        } else if (size < stack.length / 4) {
+            newSize = stack.length/2;
+        } else {
+            return;
+        }
+        T[] temp = (T[]) new Object[newSize];
+        for (int i = 0; i < Math.min(temp.length, stack.length); i++) {
+            temp[i] = stack[i];
+        }
+        size = size();
+        stack = temp;
+    }
     // tests if this stack is empty
     public boolean empty()
     {
-        return false;
+        checkSize();
+        return stack[0]==null;
     }
 
     // looks at the object at the top of this stack
     // without removing it from the stack
     public T peek()
     {
-        return null;
+        checkSize();
+        if (empty()) {
+            throw new StackUnderflowException();
+        }
+        for (int i = 0; i < stack.length; i++) {
+            if(stack[i]==null) {
+                return stack[i-1];
+            }
+        }
+        return stack[stack.length-1];
     }
 
     // removes the object at the top of this stack 
     // and returns that object as the value of this function
     public T pop()
     {
-        return null;
+        checkSize();
+        T returnT;
+        if(empty()) {
+            throw new StackUnderflowException();
+        }
+        for (int i = 0; i < stack.length; i++) {
+            if(stack[i]==null) {
+                returnT = stack[i-1];
+                stack[i-1] = null;
+                return returnT;
+            }
+        }
+        returnT = stack[stack.length - 1];
+        stack[stack.length - 1] = null;
+        size = size - 1;
+        return returnT;
     }
 
     // pushes an item onto the top of this stack
     public T push(T item)
     {
-        return null;
+        size+=1;
+        checkSize();
+
+        for (int i = 0; i < stack.length; i++) {
+            if (stack[i]==null) {
+                stack[i] = item;
+                break;
+            }
+        }
+        return item;
     }
 
     // removes all of the elements from this stack
     public void clear()
     {
+        size = 0;
+        for (int i = 0; i < stack.length; i++) {
+            stack[i] = null;
+        }
+        System.out.println(Arrays.toString(stack));
+        checkSize();
     }
 
     // returns the 1 based position where an object is on this stack
     // note: when the method ends, the stack is the same as it was at the start
     public int search(Object o)
     {
-        return -2;
+        for (int i = 0; i < size(); i++) {
+            if (stack[i].equals((T)o)) {
+                System.out.println(stack[i]);
+                System.out.println(o);
+                System.out.println(Arrays.toString(stack));
+                System.out.println(size() - i);
+                return size() - i;
+            }
+        }
+        return -1;
+
     }
 }

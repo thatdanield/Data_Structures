532939698accc455ffb1ede9dca79ab49ccc146c
thatdanield <thatdanield@gmail.com>
2022-09-28 10:37:31 -0500

    LinkedStack completed

--- src/LinkedStack.java
+++ src/LinkedStack.java
@@ -18,44 +18,74 @@ public class LinkedStack<T> implements StackInterface<T>
     // returns the logical size of the stack
     public int size()    
     {
-        return -1;
+        int count = 0;
+        LLNode<T> node = top;
+        while(node!=null) {
+            count++;
+            node = node.getLink();
+        }
+        return count;
     }
 
     // tests if this stack is empty
     public boolean empty()
     {
-        return false;
+        return top==null;
     }
 
     // looks at the object at the top of this stack
     // without removing it from the stack
     public T peek()
     {
-        return null;
+        if(size()==0) {
+            throw new StackUnderflowException();
+        }
+        return top.getInfo();
     }
 
     // removes the object at the top of this stack 
     // and returns that object as the value of this function
     public T pop()
     {
-        return null;
+        if(size()==0) {
+            throw new StackUnderflowException();
+        }
+        T info = top.getInfo();
+        top = top.getLink();
+        return info;
     }
 
     // pushes an item onto the top of this stack
     public T push(T item)
     {
-        return null;
+        LLNode<T> oldTop = top;
+        LLNode<T> newTop = new LLNode<T>(item);
+        newTop.setLink(oldTop);
+        top = newTop;
+        return item;
     }
 
     // removes all of the elements from this stack
     public void clear()
     {
+        top.setLink(null);
+        top = null;
     }
 
     // returns the 1 based position where an object is on this stack
     // note: when the method ends, the stack is the same as it was at the start
     public int search(Object o)
     {
-        return -2;
+        int count = 1;
+        LLNode<T> node = top;
+        while(!node.getInfo().equals(o)) {
+            System.out.println(node.getInfo() + " " + o);
+            node = node.getLink();
+            count++;
+            if (node==null) {
+                return -1;
+            }
+        }
+        return count;
     }
 }

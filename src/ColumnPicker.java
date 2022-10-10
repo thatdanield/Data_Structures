0740759ce95e8cd1220fb500e67bf57d091a24d7
thatdanield <thatdanield@gmail.com>
2022-10-10 13:11:27 -0500

    Starting ColumnPicker

---
+++ src/ColumnPicker.java
@@ -0,0 +1,20 @@
+import kareltherobot.*;
+/**
+ * Counts the number of beepers in a column and puts the same
+ * number of beepers at the base of the column
+ * 
+ * @author 
+ * @version 
+ */
+public class ColumnPicker extends Robot
+{
+    public ColumnPicker(int street, int avenue, Direction direction, int beepers)
+    {
+        super(street, avenue, direction, beepers);
+    }
+    
+    public void countColumns()
+    {
+    }
+    
+}

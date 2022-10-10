3860046f2f0c81cb196184726288d84cd7611b84
thatdanield <thatdanield@gmail.com>
2022-10-10 13:11:07 -0500

    Starting ColumnPicker

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,17 @@
 import kareltherobot.*;
+
 public class Driver
 {
     public static void main(String[] args)
     {
         World.setVisible(true);
-        //World.reset();
-        World.readWorld("/Users/ddevadas/IdeaProjects/Data_Structures/src", "BreadCrumbs.kwld");
-        World.setDelay(2);
+        World.reset();
+        World.readWorld("RandomColumns.kwld");
+        World.setDelay(1);
         World.setTrace(false);
-        try {Thread.sleep(3000); } catch (Exception e){};
+        try {Thread.sleep(8000); } catch (Exception e){};
 
-        BreadCrumbFollower gretel = new BreadCrumbFollower(1,1,Directions.East,0);
-        gretel.travel();
-        gretel.turnOff();
+        ColumnPicker carol = new ColumnPicker(1,1,Directions.North,-1);
+        carol.countColumns();
     }
 }

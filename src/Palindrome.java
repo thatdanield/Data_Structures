55704d1cb22110f9ef7c4807daeb37bb9daa0e5c
thatdanield <thatdanield@gmail.com>
2022-11-02 10:48:27 -0500

    Palindrome Complete

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -9,9 +9,7 @@ public class Palindrome {
         }
         int length = newWord.length();
         newWord = newWord.toLowerCase();
-        System.out.println(newWord);
         for (int i = 0; i < length/2; i++) {
-            System.out.println(newWord.substring(i, i+1) + " " + newWord.substring(length-i-1, length-i));
             if(newWord.charAt(i) != newWord.charAt(length-i-1)) {
                 return false;
             }
